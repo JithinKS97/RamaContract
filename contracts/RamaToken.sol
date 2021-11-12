738fe1db39f8a5ea3fdda6d884297193dcccfd1c
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-12 16:43:57 +0530

    film owner add

--- contracts/RamaToken.sol
+++ contracts/RamaToken.sol
@@ -1,10 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
 contract RamaToken is ERC20 {
+    address public owner;
+    using EnumerableSet for EnumerableSet.AddressSet;
+    EnumerableSet.AddressSet private filmOwners;
+
     constructor(uint256 initialSupply) ERC20("Picturama", "RAMA") {
         _mint(msg.sender, initialSupply);
+        owner = msg.sender;
+    }
+
+    modifier isOwner() {
+        assert(owner == msg.sender);
+        _;
+    }
+
+    function addFilmOwner(address newOwner) public isOwner {
+        filmOwners.add(newOwner);
+    }
+
+    function isOwnerAdded(address ownerAddress) public view returns (bool) {
+        return filmOwners.contains(ownerAddress);
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -4,13 +4,35 @@ import { ethers } from "hardhat";
 const initialSupply = 1000;
 
 describe("Rama contract tests", function () {
-  it("tests basic aspects of the token", async function () {
+  it("tests initial supply is transferred to owner during deploy", async function () {
     const [owner] = await ethers.getSigners();
+
     const RamaTokenFactory = await ethers.getContractFactory("RamaToken");
     const ramaTokenContract = await RamaTokenFactory.deploy(initialSupply);
+
     await ramaTokenContract.deployed();
+
     expect(await ramaTokenContract.balanceOf(owner.address)).to.eq(
       initialSupply
     );
+  });
+
+  it("tests addition of a film owner", async function () {
+    const [, addr1] = await ethers.getSigners();
+
+    const RamaTokenFactory = await ethers.getContractFactory("RamaToken");
+    const ramaTokenContract = await RamaTokenFactory.deploy(initialSupply);
+
+    await ramaTokenContract.addFilmOwner(addr1.address);
+    expect(await ramaTokenContract.isOwnerAdded(addr1.address)).to.eq(true);
+  });
+
+  it("tests checks if isOwnerAdded returns false if owner is not added", async function () {
+    const [, addr1] = await ethers.getSigners();
+
+    const RamaTokenFactory = await ethers.getContractFactory("RamaToken");
+    const ramaTokenContract = await RamaTokenFactory.deploy(initialSupply);
+
+    expect(await ramaTokenContract.isOwnerAdded(addr1.address)).to.eq(false);
   });
 });
